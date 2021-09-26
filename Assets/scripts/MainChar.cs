9221c551adbbbad26095034c794aad23a0a53d5a
Nimesh Shrestha <nimesh.shrestha@hotmail.com>
2021-09-26 01:59:11 -0700

    fix camera in intro animation

--- Assets/scripts/MainChar.cs
+++ Assets/scripts/MainChar.cs
@@ -7,14 +7,32 @@ public class MainChar : MonoBehaviour
     // Start is called before the first frame update
     public Rigidbody2D player;
     public float moveSpeed = 10f;
+    public GameController controller;
     // Update is called once per frame
-    void Start ()
+    void Start()
     {
         player = GetComponent<Rigidbody2D>();
+    }
+
+    private void Awake()
+    {
+        if (controller == null)
+        {
+            controller = GameObject.FindWithTag("gameController").GetComponent<GameController>();
+        }
     }
     void Update()
     {
         player.velocity = new Vector3(moveSpeed * Input.GetAxis("Horizontal"), moveSpeed * Input.GetAxis("Vertical"), 0f);
 
+    }
+
+    private void LateUpdate()
+    {
+        if (controller.gameStarted)
+        {
+
+            Camera.main.transform.position = transform.position - new Vector3(0, 0, 10);
+        }
     }
 }
